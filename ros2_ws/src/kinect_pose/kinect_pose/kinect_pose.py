fe4215dc49bcf68ce246409f7987afe8fcd6f30a
Jake Palandri <jake.palandri@gmail.com>
2024-06-26 16:20:44 +1000

    world coordinate vectors calculated correctly

--- ros2_ws/src/kinect_pose/kinect_pose/kinect_pose.py
+++ ros2_ws/src/kinect_pose/kinect_pose/kinect_pose.py
@@ -5,7 +5,7 @@ import math
 import numpy as np
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CameraInfo
 from message_filters import TimeSynchronizer, Subscriber
 import numpy as np
 from cv_bridge import CvBridge
@@ -35,80 +35,64 @@ class ReadKinectPose(Node):
         super().__init__('read_kinect_pose')
         self.model = YOLO("yolov8m-pose.pt")
         self.model = self.model.to("cuda" if torch.cuda.is_available() else "cpu")
-        colour_image = Subscriber(self, Image, "/kinect2/sd/image_color_rect")
-        depth_image = Subscriber(self, Image, "/kinect2/sd/image_depth")
-        self.tss = TimeSynchronizer([colour_image, depth_image], 10)
+        # using HD camera, needs calibrating
+        color_image = Subscriber(self, Image, "/kinect2/hd/image_color_rect")
+        depth_image = Subscriber(self, Image, "/kinect2/hd/image_depth_rect")
+        camera_info = Subscriber(self, CameraInfo, "/kinect2/hd/camera_info")
+        self.tss = TimeSynchronizer([color_image, depth_image, camera_info], 10)
         self.tss.registerCallback(self.listener_callback)
 
-    def listener_callback(self, image, depth):
+    def listener_callback(self, image, depth, info):
         stream = bridge.imgmsg_to_cv2(image, "bgr8")
-        depth = bridge.imgmsg_to_cv2(depth, "passthrough")
+        depth  = bridge.imgmsg_to_cv2(depth, "passthrough")
+        rect_matrix = np.array(info.k).reshape(3, 3)
         results = self.model(source=stream, show=True)
         isPerson = results[0].keypoints.has_visible
         result_keypoint = results[0].keypoints.xy.cpu().numpy()[0]
+
         if (not isPerson):
             print("No person detected")
             return
+        
         # LOGIC:
         # 1. Get the coordinates of the person's right wrist, elbow and hip
-        right_wrist = result_keypoint[Point.RIGHT_WRIST]
-        right_elbow = result_keypoint[Point.RIGHT_ELBOW]
-        right_hip = result_keypoint[Point.RIGHT_HIP]
+        right_wrist_2d = result_keypoint[Point.RIGHT_WRIST]
+        right_elbow_2d = result_keypoint[Point.RIGHT_ELBOW]
+        right_hip_2d   = result_keypoint[Point.RIGHT_HIP  ]
+
         # 2. Get the coordinates of the person's left wrist, elbow and hip
-        left_wrist = result_keypoint[Point.LEFT_WRIST]
-        left_elbow = result_keypoint[Point.LEFT_ELBOW]
-        left_hip = result_keypoint[Point.LEFT_HIP]
-        # 3. Get depth data at the left and right wrist elbow and hip with some margin of error (maybe check the closest depth value (not 0) within a range)
-        diameter = 5
-        min_depth_right_elbow = math.inf
-        min_depth_right_wrist = math.inf
-        min_depth_right_hip = math.inf
-        min_depth_left_elbow = math.inf
-        min_depth_left_wrist = math.inf
-        min_depth_left_hip = math.inf
-        for i in range(-math.floor(diameter/2), math.ceil(diameter/2)):
-            for j in range(-math.floor(diameter/2), math.ceil(diameter/2)):
-                rounded_right_wrist = [ round(elem) for elem in right_wrist ]
-                rounded_right_elbow = [ round(elem) for elem in right_elbow ]
-                rounded_right_hip = [ round(elem) for elem in right_hip ]
-                rounded_left_wrist = [ round(elem) for elem in left_wrist ]
-                rounded_left_elbow = [ round(elem) for elem in left_elbow ]
-                rounded_left_hip = [ round(elem) for elem in left_hip ]
-                depth_right_wrist = depth[rounded_right_wrist[1] + i][rounded_right_wrist[0] + j]
-                depth_right_elbow = depth[rounded_right_elbow[1] + i][rounded_right_elbow[0] + j]
-                depth_right_hip = depth[rounded_right_hip[1] + i][rounded_right_hip[0] + j]
-                depth_left_wrist = depth[rounded_left_wrist[1] + i][rounded_left_wrist[0] + j]
-                depth_left_elbow = depth[rounded_left_elbow[1] + i][rounded_left_elbow[0] + j]
-                depth_left_hip = depth[rounded_left_hip[1] + i][rounded_left_hip[0] + j]
-                if (depth_right_wrist < min_depth_right_wrist and depth_right_wrist != 0):
-                    min_depth_right_wrist = depth_right_wrist
-                if (depth_left_hip < min_depth_left_hip and depth_left_hip != 0):
-                    min_depth_left_hip = depth_left_hip
-                if (depth_right_hip < min_depth_right_hip and depth_right_hip != 0):
-                    min_depth_right_hip = depth_right_hip
-                if (depth_left_elbow < min_depth_left_elbow and depth_left_elbow != 0):
-                    min_depth_left_elbow = depth_left_elbow
-                if (depth_left_wrist < min_depth_left_wrist and depth_left_wrist != 0):
-                    min_depth_left_wrist = depth_left_wrist
-                if (depth_right_elbow < min_depth_right_elbow and depth_right_elbow != 0):
-                    min_depth_right_elbow = depth_right_elbow
-        # 4. Convert the 2d coordinates to 3d coordinates with Z up and Y depth
-        right_wrist = np.array([right_wrist[0], min_depth_right_wrist, right_wrist[1]])
-        right_elbow = np.array([right_elbow[0], min_depth_right_elbow, right_elbow[1]])
-        right_hip = np.array([right_hip[0], min_depth_right_hip, right_hip[1]])
-        left_wrist = np.array([left_wrist[0], min_depth_left_wrist, left_wrist[1]])
-        left_elbow = np.array([left_elbow[0], min_depth_left_elbow, left_elbow[1]])
-        left_hip = np.array([left_hip[0], min_depth_left_hip, left_hip[1]])
+        left_wrist_2d  = result_keypoint[Point.LEFT_WRIST ]
+        left_elbow_2d  = result_keypoint[Point.LEFT_ELBOW ]
+        left_hip_2d    = result_keypoint[Point.LEFT_HIP   ]
+
+        # 3. Get depth data at the left and right wrist elbow and hip with some margin of error
+        right_wrist_z = self.min_depth(depth, right_wrist_2d)
+        right_elbow_z = self.min_depth(depth, right_elbow_2d)
+        right_hip_z   = self.min_depth(depth, right_hip_2d  )
+        left_wrist_z  = self.min_depth(depth, left_wrist_2d )
+        left_elbow_z  = self.min_depth(depth, left_elbow_2d )
+        left_hip_z    = self.min_depth(depth, left_hip_2d   )
+
+        # 4. Convert 2d pixel coordinates to 3d world coordinates
+        right_wrist_3d = self.pixel_to_world(right_wrist_2d, rect_matrix, right_wrist_z)
+        right_elbow_3d = self.pixel_to_world(right_elbow_2d, rect_matrix, right_elbow_z)
+        right_hip_3d   = self.pixel_to_world(right_hip_2d  , rect_matrix, right_hip_z  )
+        left_wrist_3d  = self.pixel_to_world(left_wrist_2d , rect_matrix, left_wrist_z )
+        left_elbow_3d  = self.pixel_to_world(left_elbow_2d , rect_matrix, left_elbow_z )
+        left_hip_3d    = self.pixel_to_world(left_hip_2d   , rect_matrix, left_hip_z   )
+
         # 5. Calculate the 3d vector for the left and right forearms
-        right_vector = right_wrist - right_elbow
-        left_vector = left_wrist - left_elbow
+        right_vector = right_wrist_3d - right_elbow_3d
+        left_vector  = left_wrist_3d  - left_elbow_3d
         
-        # 7. Determine which arm is more extended
-        # 7a. Determine distance from right wrist to right hip
-        right_distance = np.linalg.norm(right_wrist - right_hip)
-        # 7b. Determine distance from left wrist to left hip
-        left_distance = np.linalg.norm(left_wrist - left_hip)
-        # 7c. Compare the distances
+        # 6. Determine which arm is more extended
+        # 6a. Determine distance from right wrist to right hip
+        right_distance = np.linalg.norm(right_wrist_3d - right_hip_3d)
+
+        # 6b. Determine distance from left wrist to left hip
+        left_distance = np.linalg.norm(left_wrist_3d - left_hip_3d)
+
+        # 6c. Compare the distances
         # NEED A MORE ADVANCED WAY TO DO THIS
         # NEED A MINIMUM EXTENSION THRESHOLD
         # Default to right arm
@@ -120,8 +104,29 @@ class ReadKinectPose(Node):
         else:
             print("Right arm is more extended")
             print(vector)
-        # 10. Extend the vector to the ends of the room
-        # 11. Determine what the person is pointing at with some margin of error
+
+        # 7. Extend the vector to the ends of the room
+        # 8. Determine what the person is pointing at with some margin of error
+    
+    def min_depth(self, depth, keypoint, diameter=5):
+        x, y = map(round, keypoint)
+        min_depth = math.inf
+
+        for dy in range(-diameter//2, diameter//2 + 1):
+            for dx in range(-diameter//2, diameter//2 + 1):
+                if (0 <= x + dx < depth.shape[1] and
+                    0 <= y + dy < depth.shape[0]):
+                    d = depth[y + dy][x + dx]
+                    if d != 0 and d < min_depth:
+                        min_depth = d
+        return min_depth
+
+    def pixel_to_world(self, pixel, rect_matrix, depth):
+        pixel_homogeneous = np.array([pixel[0], pixel[1], 1])
+        world_coords = np.matmul(np.linalg.inv(rect_matrix), pixel_homogeneous)
+        world_coords *= depth
+        world_coords[1] *= -1 # make y positive up
+        return world_coords
 
 
 def main(args=None):
